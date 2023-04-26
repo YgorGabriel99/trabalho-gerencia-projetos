0c304275223c5c1745e4a4965910e0ae72505c17
Paulo Vitor <paulonovaes@id.uff.br>
2023-04-25 23:17:15 -0300

    instanciando countries

--- src/first-scene.ts
+++ src/first-scene.ts
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
-
+//import { createContadorImagem } from '/services/country.service';
+import {createContadorImagem} from './services/country-services.ts';
 /**
  * FirstGameScene is an example Phaser Scene
  * @class
@@ -29,6 +30,7 @@ export class FirstGameScene extends Phaser.Scene {
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
         this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.image('circle', 'assets/score.png')
     }
 
     create() {
@@ -38,7 +40,23 @@ export class FirstGameScene extends Phaser.Scene {
         let altura = window.screen.height;
         let mapa = this.add.image(largura/2, altura/3, 'mapa');
         mapa.setScale(0.8);
-        
+
+        createContadorImagem(this,485, 290);
+        createContadorImagem(this, 430, 290);
+        createContadorImagem(this, 430, 240);
+        createContadorImagem(this, 450, 340);
+        /*
+        var brasil = this.add.image(485,290, 'circle')
+        brasil.setScale(0.05,0.05)
+        brasil.setInteractive();
+        let count = 0; // contador inicial
+        let countText = this.add.text(485, 290, count.toString(), { font: '24px Arial' }); // texto para exibir o contador
+        countText.setOrigin(0.5); // define a origem do texto para o centro da imagem
+
+        brasil.on('pointerdown', () => { // adiciona um evento de clique na imagem
+            count++; // atualiza o contador
+            countText.setText(count.toString()); // atualiza o texto do contador
+        });*/
     }
 
     update() {
