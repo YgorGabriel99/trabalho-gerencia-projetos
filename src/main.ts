7ae89af957b4b6c1e6778ae0d29367b3a7d32c44
TiamatCod3 <ti_matos@id.uff.br>
2023-04-27 18:22:23 -0300

    Criação do mapa individualmente

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { FirstGameScene } from './first-scene';
 import Phaser from 'phaser';
-import './reset.css';
+// import './reset.css';
 import './style.css';
+import { MainGameScene } from './main-scene';
 
 
 const config: Phaser.Types.Core.GameConfig = {
-    width: "100%",
-    height: "100%",
+    width: 1227,
+    height: 628,
     type: Phaser.AUTO,
     parent: 'game-container',
     backgroundColor: '#B4CDFF',
-    scene: [FirstGameScene]
+    scene: [MainGameScene, FirstGameScene]
   };
 
 new Phaser.Game(config);
