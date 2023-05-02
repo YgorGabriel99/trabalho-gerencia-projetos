e379a1ab8fd99e05f57cc4ff0fb39b14b5e3898b
TiamatCod3 <ti_matos@id.uff.br>
2023-05-02 10:41:43 -0300

    Criação da renderização dos países

--- src/main-scene.ts
+++ src/main-scene.ts
@@ -1,7 +1,8 @@
 import Phaser from "phaser";
 
 export class MainGameScene extends Phaser.Scene {
-
+    SCALE = 1
+    ORIGIN = 0
     constructor() {
         super('MainGameScene');
     }
@@ -19,66 +20,86 @@ export class MainGameScene extends Phaser.Scene {
     create():void{
         let data = this.cache.json.get('frame');
 
-        //América do Sul
-        let brasil = this.add.sprite(0,0, 'territorios', 'brasil').setOrigin(0,0).setScale(0.5)
-        // console.log(brasil.frame.name) 
-        let venezuela = this.add.sprite(0,0, 'territorios', 'venezuela').setOrigin(0).setScale(0.5) 
-        let argentina = this.add.sprite(0,0, 'territorios', 'argentina').setOrigin(0).setScale(0.5)  
-        let bolivia = this.add.sprite(0,0, 'territorios', 'bolivia').setOrigin(0).setScale(0.5) 
-        
-        //Europa
-        let alemanha = this.add.sprite(0,0, 'territorios', 'alemanha').setOrigin(0).setScale(0.5) 
-        let franca = this.add.sprite(0,0, 'territorios', 'franca').setOrigin(0).setScale(0.5) 
-        let islandia = this.add.sprite(0,0, 'territorios', 'islandia').setOrigin(0).setScale(0.5)
-        let suecia = this.add.sprite(0,0, 'territorios', 'suecia').setOrigin(0).setScale(0.5) 
-        let italia = this.add.sprite(0,0, 'territorios', 'italia').setOrigin(0).setScale(0.5)  
-        let moscou = this.add.sprite(0,0, 'territorios', 'moscou').setOrigin(0).setScale(0.5)
-        
-        //África
-        let sudao = this.add.sprite(0,0, 'territorios', 'sudao').setOrigin(0).setScale(0.5) 
-        let congo = this.add.sprite(0,0, 'territorios', 'congo').setOrigin(0).setScale(0.5) 
-        let argelia = this.add.sprite(0,0, 'territorios', 'argelia').setOrigin(0).setScale(0.5)
-        let madasgacar = this.add.sprite(0,0, 'territorios', 'madagascar').setOrigin(0).setScale(0.5)  
-        let egito = this.add.sprite(0,0, 'territorios', 'egito').setOrigin(0).setScale(0.5) 
-        let africa_do_sul = this.add.sprite(0,0, 'territorios', 'africa-do-sul').setOrigin(0).setScale(0.5) 
+        // console.log(typeof(data.frames))
+        // console.log(data.frames)
+        // data.frames.map(element =>{
+        //     console.log(element)
+        // })
 
-        //América do Norte
-        let groenlandia = this.add.sprite(0,0, 'territorios', 'groenlandia').setOrigin(0).setScale(0.5) 
-        let mackenzie = this.add.sprite(0,0, 'territorios', 'mackenzie').setOrigin(0,0).setScale(0.5)
-        let inglaterra = this.add.sprite(0,0, 'territorios', 'inglaterra').setOrigin(0).setScale(0.5) 
-        let ottawa = this.add.sprite(0,0, 'territorios', 'ottawa').setOrigin(0).setScale(0.5) 
-        let phaser = this.add.sprite(0,0, 'territorios', 'phaser').setOrigin(0).setScale(0.5) 
-        let nova_iorque = this.add.sprite(0,0, 'territorios', 'nova-iorque').setOrigin(0).setScale(0.5)
-        let mexico = this.add.sprite(0,0, 'territorios', 'mexico').setOrigin(0).setScale(0.5) 
-        let california = this.add.sprite(0,0, 'territorios', 'california').setOrigin(0).setScale(0.5)  
-        let vancouver = this.add.sprite(0,0, 'territorios', 'vancouver').setOrigin(0).setScale(0.5)
-        let labrador = this.add.sprite(0,0, 'territorios', 'labrador').setOrigin(0).setScale(0.5) 
-        let alasca = this.add.sprite(0,0, 'territorios', 'alasca').setOrigin(0).setScale(0.5) 
+        for(territory of data.frames.toArray()){
+            console.log(territory)
+        }
+        
+        // //América do Sul
+        // let brasil = this.add.sprite(0,0, 'territorios', 'brasil').setOrigin(this.ORIGIN).setScale(this.SCALE)
+        // console.log(brasil.frame)
+        // this.add.bitmapText(
+        //     brasil.frame.x + brasil.frame.halfHeight, 
+        //     brasil.frame.halfWidth + brasil.frame.y, 'pressstart', brasil.frame.name,15,1).setTint(0xeeeeee)
+
+        // let venezuela = this.add.sprite(0,0, 'territorios', 'venezuela').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let argentina = this.add.sprite(0,0, 'territorios', 'argentina').setOrigin(this.ORIGIN).setScale(this.SCALE)  
+        // let bolivia = this.add.sprite(0,0, 'territorios', 'bolivia').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        
+        // //Europa
+        // let alemanha = this.add.sprite(0,0, 'territorios', 'alemanha').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let franca = this.add.sprite(0,0, 'territorios', 'franca').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let islandia = this.add.sprite(0,0, 'territorios', 'islandia').setOrigin(this.ORIGIN).setScale(this.SCALE)
+        // let suecia = this.add.sprite(0,0, 'territorios', 'suecia').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let italia = this.add.sprite(0,0, 'territorios', 'italia').setOrigin(this.ORIGIN).setScale(this.SCALE)  
+        // let moscou = this.add.sprite(0,0, 'territorios', 'moscou').setOrigin(this.ORIGIN).setScale(this.SCALE)
+        
+        // //África
+        // let sudao = this.add.sprite(0,0, 'territorios', 'sudao').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let congo = this.add.sprite(0,0, 'territorios', 'congo').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let argelia = this.add.sprite(0,0, 'territorios', 'argelia').setOrigin(this.ORIGIN).setScale(this.SCALE)
+        // let madasgacar = this.add.sprite(0,0, 'territorios', 'madagascar').setOrigin(this.ORIGIN).setScale(this.SCALE)  
+        // let egito = this.add.sprite(0,0, 'territorios', 'egito').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let africa_do_sul = this.add.sprite(0,0, 'territorios', 'africa-do-sul').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+
+        // //América do Norte
+        // let groenlandia = this.add.sprite(0,0, 'territorios', 'groenlandia').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let mackenzie = this.add.sprite(0,0, 'territorios', 'mackenzie').setOrigin(this.ORIGIN).setScale(this.SCALE)
+        // let inglaterra = this.add.sprite(0,0, 'territorios', 'inglaterra').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let ottawa = this.add.sprite(0,0, 'territorios', 'ottawa').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let phaser = this.add.sprite(0,0, 'territorios', 'phaser').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let nova_iorque = this.add.sprite(0,0, 'territorios', 'nova-iorque').setOrigin(this.ORIGIN).setScale(this.SCALE)
+        // let mexico = this.add.sprite(0,0, 'territorios', 'mexico').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let california = this.add.sprite(0,0, 'territorios', 'california').setOrigin(this.ORIGIN).setScale(this.SCALE)  
+        // let vancouver = this.add.sprite(0,0, 'territorios', 'vancouver').setOrigin(this.ORIGIN).setScale(this.SCALE)
+        // let labrador = this.add.sprite(0,0, 'territorios', 'labrador').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let alasca = this.add.sprite(0,0, 'territorios', 'alasca').setOrigin(this.ORIGIN).setScale(this.SCALE) 
          
-        //Ásia
-        let aral = this.add.sprite(0,0, 'territorios', 'aral').setOrigin(0).setScale(0.5) 
-        let siberia = this.add.sprite(0,0, 'territorios', 'siberia').setOrigin(0).setScale(0.5) 
-        let india = this.add.sprite(0,0, 'territorios', 'india').setOrigin(0).setScale(0.5) 
-        let vietna = this.add.sprite(0,0, 'territorios', 'vietna').setOrigin(0).setScale(0.5)
-        let china = this.add.sprite(0,0, 'territorios', 'china').setOrigin(0).setScale(0.5) 
-        let mongolia = this.add.sprite(0,0, 'territorios', 'mongolia').setOrigin(0).setScale(0.5)  
-        let vladivostok = this.add.sprite(0,0, 'territorios', 'vladivostok').setOrigin(0).setScale(0.5) 
-        let dudinka = this.add.sprite(0,0, 'territorios', 'dudinka').setOrigin(0).setScale(0.5) 
-        let omsk = this.add.sprite(0,0, 'territorios', 'omsk').setOrigin(0).setScale(0.5) 
-        let tchita = this.add.sprite(0,0, 'territorios', 'tchita').setOrigin(0).setScale(0.5) 
-        let japao = this.add.sprite(0,0, 'territorios', 'japao').setOrigin(0).setScale(0.5) 
+        // //Ásia
+        // let aral = this.add.sprite(0,0, 'territorios', 'aral').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let siberia = this.add.sprite(0,0, 'territorios', 'siberia').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let india = this.add.sprite(0,0, 'territorios', 'india').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let vietna = this.add.sprite(0,0, 'territorios', 'vietna').setOrigin(this.ORIGIN).setScale(this.SCALE)
+        // let china = this.add.sprite(0,0, 'territorios', 'china').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let mongolia = this.add.sprite(0,0, 'territorios', 'mongolia').setOrigin(this.ORIGIN).setScale(this.SCALE)  
+        // let vladivostok = this.add.sprite(0,0, 'territorios', 'vladivostok').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let dudinka = this.add.sprite(0,0, 'territorios', 'dudinka').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let omsk = this.add.sprite(0,0, 'territorios', 'omsk').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let tchita = this.add.sprite(0,0, 'territorios', 'tchita').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let japao = this.add.sprite(0,0, 'territorios', 'japao').setOrigin(this.ORIGIN).setScale(this.SCALE) 
 
-        //Oceania
-        let australia = this.add.sprite(0,0, 'territorios', 'australia').setOrigin(0).setScale(0.5) 
-        let nova_guine = this.add.sprite(0,0, 'territorios', 'nova-guine').setOrigin(0).setScale(0.5) 
-        let sumatra = this.add.sprite(0,0, 'territorios', 'sumatra').setOrigin(0).setScale(0.5) 
-        let borneo = this.add.sprite(0,0, 'territorios', 'borneo').setOrigin(0).setScale(0.5) 
-        // let mackenzie = this.add.sprite(0,0, 'territorios', 'mackenzie').setOrigin(0) 
-        // let argentina = this.add.sprite(0,0, 'territorios', 'argentina').setOrigin(0) 
-        // let font = this.add.bitmapText(0, 0, 'pressstart', 'Canção de war')
+        // //Oceania
+        // let australia = this.add.sprite(0,0, 'territorios', 'australia').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let nova_guine = this.add.sprite(0,0, 'territorios', 'nova-guine').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let sumatra = this.add.sprite(0,0, 'territorios', 'sumatra').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // let borneo = this.add.sprite(0,0, 'territorios', 'borneo').setOrigin(this.ORIGIN).setScale(this.SCALE) 
+        // // let mackenzie = this.add.sprite(0,0, 'territorios', 'mackenzie').setOrigin(0) 
+        // // let argentina = this.add.sprite(0,0, 'territorios', 'argentina').setOrigin(0) 
+        // let font = this.add.bitmapText(100, 100, 'pressstart', 'War Game')
 
         
         // console.log(data.frames['brasil'])
+        // console.log(data)
+        // brasil.setAlpha(0.5)
+        // brasil.setTint('#c3c3c3')
+        this.add.bitmapText(350, 300, 'pressstart', 'Oceano \n Atlântico',15,1)
+        this.add.bitmapText(800, 350, 'pressstart', 'Oceano \n Índico',15,1)
+        this.add.bitmapText(100, 400, 'pressstart', 'Oceano \n Pacífico',15,1)
         
 
     }
